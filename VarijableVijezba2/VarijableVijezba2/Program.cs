d2cdfb118f08da726c101098a5845371aeff7b37
Neven <58472164+neven14@users.noreply.github.com>
2020-11-09 08:56:04 +0100

    Add files via upload

---
+++ VarijableVijezba2/VarijableVijezba2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarijableVijezba2
+{
+    class Program
+    { 
+    
+
+       public static void Main()
+        {
+            byte broj = 100;
+            int broj1 = -1000;
+            char broj2 = "a";
+            float broj3 = 3.123;
+            double broj4 = 3.123456789;
+            bool broj5 = true;
+            string broj6 = "true";
+
+            Console.WriteLine("{0}, {1} , {2} , {3} , {4} , {5} , {6}", broj, broj1, broj2, broj3, broj4, broj5, broj6);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
